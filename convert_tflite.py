02ed3ff61430918eb8e54fc2affa6251d9c4ef52
Dragos Daian <daian.dragos@yahoo.com>
2024-08-13 13:42:52 +0200

    Update convert_tflite.py

--- convert_tflite.py
+++ convert_tflite.py
@@ -9,17 +9,17 @@ import os
 
 
 def setup_gdscript(model, url):
-    interpreter = tf.lite.Interpreter(model_path=model)
-    interpreter.allocate_tensors()
-    input_details = interpreter.get_input_details()
-    output_details = interpreter.get_output_details()
-    inputs = ""
-    outputs = ""
-    for input_detail in input_details:
-        inputs += f"## {input_detail['name']}: {input_detail['dtype']} {input_detail['shape']}\n"
-    for output_detail in output_details:
-        outputs += f"\n## {output_detail['name']}: {output_detail['dtype']} {output_detail['shape']}"
-    gdscript_file = f"""@icon("res://addons/iree-gd/logo.svg")
+	interpreter = tf.lite.Interpreter(model_path=model)
+	interpreter.allocate_tensors()
+	input_details = interpreter.get_input_details()
+	output_details = interpreter.get_output_details()
+	inputs = ""
+	outputs = ""
+	for input_detail in input_details:
+		inputs += f"## {input_detail['name']}: {input_detail['dtype']} {input_detail['shape']}\n"
+	for output_detail in output_details:
+		outputs += f"\n## {output_detail['name']}: {output_detail['dtype']} {output_detail['shape']}"
+	gdscript_file = f"""@icon("res://addons/iree-gd/logo.svg")
 extends IREERunner
 class_name IREEModule_{url}
 
@@ -41,30 +41,30 @@ func _load_module() -> IREEModule:
 func main(inputs: Array[IREETensor]) -> IREEResult:
 	return run("module.main", inputs)
 """
-    with open(f"build/{url}.gd", "w") as f:
-        f.write(gdscript_file)
+	with open(f"build/{url}.gd", "w") as f:
+		f.write(gdscript_file)
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Enter model url")
-    parser.add_argument("url", type=str, help="The model url from kaggle")
-    args = parser.parse_args()
-    path = kaggle_download.model_download(args.url)
-    targets = ['llvm-cpu', 'metal-spirv', 'vulkan-spirv']
-    print("Building for: ", targets)
-    # Find .tflite file
-    for root, dirs, files in os.walk(path):
-        for file in files:
-            if file.endswith(".tflite"):
-                path = os.path.join(root, file)
-                break
-    print("Path to model file:", path)
-    folder_name = args.url.replace('/', '_').replace('-', '_')
-    with open(f"model_name.txt", "w") as f:
-        f.write(folder_name)
-    os.makedirs(f"build", exist_ok=True)
-    # First generate inputs/outputs
-    setup_gdscript(path, folder_name)
-    for target in targets:
+	parser = argparse.ArgumentParser(description="Enter model url")
+	parser.add_argument("url", type=str, help="The model url from kaggle")
+	args = parser.parse_args()
+	path = kaggle_download.model_download(args.url)
+	targets = ['llvm-cpu', 'metal-spirv', 'vulkan-spirv']
+	print("Building for: ", targets)
+	# Find .tflite file
+	for root, dirs, files in os.walk(path):
+		for file in files:
+			if file.endswith(".tflite"):
+				path = os.path.join(root, file)
+				break
+	print("Path to model file:", path)
+	folder_name = args.url.replace('/', '_').replace('-', '_')
+	with open(f"model_name.txt", "w") as f:
+		f.write(folder_name)
+	os.makedirs(f"build", exist_ok=True)
+	# First generate inputs/outputs
+	setup_gdscript(path, folder_name)
+	for target in targets:
 		print("Compiling for: ", target)
 		iree_tflite_compile.compile_file(
 			path,
